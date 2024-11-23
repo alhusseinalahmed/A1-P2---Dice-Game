5c0f48f1c46911dc6f12e0e10b18cc8efec57347
Al Hussein Al Ahmed <aa227hr@student.lnu.se>
2024-11-23 11:08:35 +0100

    GREEN

---
+++ src/main/java/com/example/dicegame/Player.java
@@ -0,0 +1,27 @@
+package com.example.dicegame;
+
+public class Player {
+
+  private String name;
+  private int score;
+
+  public Player(String name) {
+    setName(name);
+    setScore(0);
+  }
+  public String getName() {
+    return name;
+  }
+
+  private void setName(String name) {
+    this.name = name;
+  }
+
+  public int getScore() {
+    return score;
+  }
+
+  private void setScore(int score) {
+    this.score = score;
+  }
+}
--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,15 +1,18 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import com.example.dicegame.Player;
 
 import org.junit.jupiter.api.BeforeEach;
 
 public class PlayerTest {
 
+  private Player player;
+
   @BeforeEach
   void setUp() {
-    Player player = new Player("John");
+    player = new Player("John");
   }
   @Test
   void playerShouldStartWithZeroScore(){
